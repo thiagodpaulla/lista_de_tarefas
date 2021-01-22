b2a926e94266a9c59df81de9c1f540c91400aa7e
thiago vieira <sesthi@gmail.com>
2021-01-22 15:45:56 -0300

    updadte

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Main from './components/Main';
 import './App.css';
 
-//criando um componete sem estado que é uma função normal que retorna uma função jsx
-
 export default function App() {
-return <Main />;
+  return <Main />;
 }
